c8334825b15f8bdb39a5d8a1a4c5f3edefc3af3b
Danilo Azevedo Nascimento <62964593+daniloazeveedo@users.noreply.github.com>
2026-04-27 08:14:49 -0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI.firebaseapp.com",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI.appspot.com",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
-};
